73a6d24f804fda60c3331cd130cdb6874aeee80e
Anderson Ferreira <andersonferreira251139@gmail.com>
2023-12-19 09:28:57 -0300

    .

--- menu-hamburguer.js
+++
@@ -1,6 +0,0 @@
-function animar(){
-  const btn = document.querySelector('#btn-menu');
-  const nav = document.querySelector('#nav')
-  btn.classList.toggle('ativo');
-  nav.classList.toggle('ativo');
-}
